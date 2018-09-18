7bdec835aaf23eab7274b583d96a988bd78c022b
Vadim Cpp <vadim.cpp@gmail.com>
2018-09-18 20:03:43 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var browserSync = require('browser-sync').create();
+var gulp = require('gulp');
+
+//
+// Configure the browserSync task
+//
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  })
+});
+
+
+
+gulp.task('watch', function() {
+    gulp.watch('index.html', browserSync.reload);
+    // gulp.watch('css/**/*', browserSync.reload);
+});
+
+
+gulp.task('default', gulp.series('browserSync', 'watch', function() { 
+    // default task code here
+}));
